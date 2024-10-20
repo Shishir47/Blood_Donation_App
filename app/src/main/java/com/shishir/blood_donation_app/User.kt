27295910c01a1c47af1c98c6575b6ac2dbeef9f2
Raufun Nasim Khan Chowdhury <madmaxshishir47@gmail.com>
2024-10-20 12:06:07 +0600

    Fixed User Not Displaying Error

--- app/src/main/java/com/shishir/blood_donation_app/User.kt
+++ app/src/main/java/com/shishir/blood_donation_app/User.kt
@@ -5,9 +5,9 @@ import com.google.firebase.firestore.PropertyName
 data class User(
     @JvmField @PropertyName("Full Name") val fullName: String = "",
     @JvmField @PropertyName("Address") val address: String = "",
-    @JvmField @PropertyName("Availablity Status") val availabilityStatus: String = "",
+    @JvmField @PropertyName("Availability Status") val availabilityStatus: String = "",
     @JvmField @PropertyName("Blood Group") val bloodGroup: String = "",
     @JvmField @PropertyName("City") val city: String = "",
     @JvmField @PropertyName("Contact Number") val contactNumber: String = "",
-    @JvmField @PropertyName("Donor Email") val donorEmail: String = ""
+    @JvmField @PropertyName("Email") val donorEmail: String = ""
 )
